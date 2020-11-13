85c5fd960284cbe216d4ea53d615b92fcb4e74c4
Rogério Duarte <rogeriotri@gmail.com>
2020-11-12 21:17:07 -0300

    Adicionado barras de navegação e rotas.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,35 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './Course';
+import { CourseService } from './course.service';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
- courses: Course[] = [];
-ngOnInit(): void{
-    this.courses = [
-        {
-            id: 1,
-            name: 'Angular: Forms',
-            imageUrl: '/assets/images/forms.png',
-            price: 99.99,
-            code: 'XPS-8796',
-            duration: 120,
-            rating: 4.5,
-            releaseDate: 'November, 2, 2019' 
-        },
-        {
-            id: 2,
-            name: 'Angular: HTTP',
-            imageUrl: '/assets/images/http.png',
-            price: 45.99,
-            code: 'LKL-1094',
-            duration: 80,
-            rating: 4, 
-            releaseDate: 'December, 4, 2019'
-        }
-    ]
-}
+
+    filteredCourses: Course [] = [];
+    _courses: Course[] = [];
+    _filterBy: string;
+
+    constructor(private courseService: CourseService){ }
+    
+    ngOnInit(): void{
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+    }
+    get filter() {
+        return this._filterBy;
+    }
 
 }
